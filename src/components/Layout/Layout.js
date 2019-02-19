0077c71dfcbfd592987946ab1dfdc5ef0da21739
steveaolsen <steveaolsen@gmail.com>
2019-02-19 18:10:36 -0500

    x

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+const layout = (props) => (
+    <Aux>
+        <Toolbar />
+        <div>SideDrawer (layout.js)</div>
+        <main className ={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
