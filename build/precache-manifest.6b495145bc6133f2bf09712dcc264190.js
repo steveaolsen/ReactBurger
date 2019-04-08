3816d5f8ddb37f6c9264ff60a2f8b64ac3e8c9ee
steveaolsen <steveaolsen@gmail.com>
2019-04-08 19:14:38 -0400

    x

---
+++ build/precache-manifest.6b495145bc6133f2bf09712dcc264190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80362383c5fee1ea0c7f",
+    "url": "/static/js/main.80362383.chunk.js"
+  },
+  {
+    "revision": "a48dfa1fb89ea2fd4aa1",
+    "url": "/static/js/1.a48dfa1f.chunk.js"
+  },
+  {
+    "revision": "80362383c5fee1ea0c7f",
+    "url": "/static/css/main.f37510d4.chunk.css"
+  },
+  {
+    "revision": "69723e51dfeffc26ab906fb38a51120f",
+    "url": "/index.html"
+  }
+];
